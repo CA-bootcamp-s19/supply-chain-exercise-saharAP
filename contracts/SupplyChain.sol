29912fe47d6157978f91cb811c0c145fa2daf207
sahar panahloo <sahar.aplo@gmail.com>
2020-10-04 15:57:28 +0330

    second commit

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -10,13 +10,14 @@ contract SupplyChain {
 
   /* set owner */
   address owner;
-
+  
+    
   /* Add a variable called skuCount to track the most recent sku # */
-
+    uint public skuCount;
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
-
+    mapping(uint=>Item) public items;
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
     Sold
@@ -24,19 +25,31 @@ contract SupplyChain {
     Received
     (declaring them in this order is important for testing)
   */
-
+    enum State{ForSale,Sold,Shipped,Received}
   /* Create a struct named Item.
     Here, add a name, sku, price, state, seller, and buyer
     We've left you to figure out what the appropriate types are,
     if you need help you can ask around :)
     Be sure to add "payable" to addresses that will be handling value transfer
   */
-
+    struct Item{
+        string  name;
+        uint    sku;
+        uint    price;
+        State   state;
+        address payable seller;
+        address payable buyer;
+    }
   /* Create 4 events with the same name as each possible State (see above)
     Prefix each event with "Log" for clarity, so the forSale event will be called "LogForSale"
     Each event should accept one argument, the sku */
-
+    event LogForSale(uint sku);
+    event LogSold(uint sku);
+    event LogShipped(uint sku);
+    event LogReceived(uint sku);
+    
 /* Create a modifer that checks if the msg.sender is the owner of the contract */
+  modifier owned () { require (msg.sender == owner); _;}
 
   modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
 
@@ -56,15 +69,21 @@ contract SupplyChain {
    so checking that Item.State == ForSale is not sufficient to check that an Item is for sale.
    Hint: What item properties will be non-zero when an Item has been added?
    */
-  modifier forSale
-  modifier sold
-  modifier shipped
-  modifier received
+  modifier forSale(uint _sku){
+      require(items[_sku].state==State.ForSale);
+      require(items[_sku].price!=0);
+      require(items[_sku].seller!=address(0));
+  _;}
+  modifier sold(uint _sku){require(items[_sku].state==State.Sold);_;}
+  modifier shipped(uint _sku){require(items[_sku].state==State.Shipped);_;}
+  modifier received(uint _sku){require(items[_sku].state==State.Received);_;}
 
 
   constructor() public {
     /* Here, set the owner as the person who instantiated the contract
        and set your skuCount to 0. */
+       owner=msg.sender;
+       skuCount=0;
   }
 
   function addItem(string memory _name, uint _price) public returns(bool){
@@ -80,21 +99,32 @@ contract SupplyChain {
     if the buyer paid enough, and check the value after the function is called to make sure the buyer is
     refunded any excess ether sent. Remember to call the event associated with this function!*/
 
-  function buyItem(uint sku)
+  function buyItem(uint _sku)payable forSale(_sku) paidEnough(items[_sku].price) checkValue(_sku)
     public
-  {}
+  {
+      items[_sku].seller.transfer(items[_sku].price);
+      items[_sku].state=State.Sold;
+      items[_sku].buyer=msg.sender;
+      emit LogSold(_sku);
+  }
 
   /* Add 2 modifiers to check if the item is sold already, and that the person calling this function
   is the seller. Change the state of the item to shipped. Remember to call the event associated with this function!*/
-  function shipItem(uint sku)
+  function shipItem(uint _sku)sold(_sku) verifyCaller(items[_sku].seller)
     public
-  {}
+  {
+      items[_sku].state=State.Shipped;
+      emit LogShipped(_sku);
+  }
 
   /* Add 2 modifiers to check if the item is shipped already, and that the person calling this function
   is the buyer. Change the state of the item to received. Remember to call the event associated with this function!*/
-  function receiveItem(uint sku)
+  function receiveItem(uint _sku)shipped(_sku) verifyCaller(items[_sku].buyer)
     public
-  {}
+  {
+      items[_sku].state=State.Received;
+      emit LogReceived(_sku);
+  }
 
   /* We have these functions completed so we can run tests, just ignore it :) */
   function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) {
--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -6,22 +6,86 @@ import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
 
+ 
     // Test for failing conditions in this contracts:
     // https://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
+       
+    SupplyChain supply = SupplyChain(DeployedAddresses.SupplyChain());
+    uint public initialBalance = 2 ether;
+    
+    uint    price   = 10000;
+    string  name    = "book";
+    uint    sku     =0;
+    uint    price1  = 20000;
+    string  name1   = "chair";
+    uint    sku1    =1;
 
+  function () external payable{
+    // This will NOT be executed when Ether is sent. \o/
+  }
     // buyItem
 
     // test for failure if user does not send enough funds
     // test for purchasing an item that is not for Sale
+    function testForFailureIfUserDoesNotSendEnoughFunds()public {
+
+        bool r;
+        supply.addItem(name,price);
+       
+        (r,)=address(supply).call.value(100)(abi.encodeWithSignature("buyItem(uint256)",sku));
+        Assert.isFalse(r, "test for failure if user does not send enough funds");
+
+       
+  }
+   function testForPurchasingAnItemThatIsNotForSale()public{
+        
+        bool r;
+
+        (r,)=address(supply).call.value(price)(abi.encodeWithSignature("buyItem(uint256)",sku));
+
+        (r,)=address(supply).call.value(price)(abi.encodeWithSignature("buyItem(uint256)",sku));
+        Assert.isFalse(r, "test for purchasing an item that is not for Sale");
+    } 
 
     // shipItem
 
     // test for calls that are made by not the seller
     // test for trying to ship an item that is not marked Sold
+    function testForCallsThatAreMadeByNotTheSeller()public{
+        bool r;
 
+        (r,)=address(supply).delegatecall(abi.encodeWithSignature("shipItem(uint256)",sku));
+        Assert.isFalse(r, "test for calls that are made by not the seller");
+
+    }
+    function testForTryingToShipAnItemThatIsNotMarkedSold()public{
+
+        bool r;
+        supply.addItem(name1,price1);
+
+        (r,)=address(supply).call(abi.encodeWithSignature("shipItem(uint256)",sku1));
+        Assert.isFalse(r, "test for trying to ship an item that is not marked Sold");
+
+    }
     // receiveItem
 
     // test calling the function from an address that is not the buyer
     // test calling the function on an item not marked Shipped
+    function testCallingTheFunctionFromAnAddressThatIsNotTheBuyer()public{
 
+        bool r;
+        
+        (r,)=address(supply).call(abi.encodeWithSignature("shipItem(uint256)",sku));
+        (r,)=address(supply).delegatecall(abi.encodeWithSignature("receiveItem(uint256)",sku));
+        Assert.isFalse(r, "test calling the function from an address that is not the buyer");
+
+    }
+    function testCallingTheFunctionOnAnItemNotMarkedShipped()public{
+
+        bool r;
+
+        (r,)=address(supply).call(abi.encodeWithSignature("receiveItem(uint256)",sku1));
+        Assert.isFalse(r, "test calling the function on an item not marked Shipped");
+
+    }
 }
